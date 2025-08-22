bc04ce2ccfbb4353096b7ae4485e055092127952
Bobbala Venkata Nagi <Bobbala Venkata Nagi@FFE-Bobbala-Venkata-Nagireddy>
2025-08-22 17:36:34 +0530

    TC_001 Committed

---
+++ src/test/java/practise/TC_001.java
@@ -0,0 +1,10 @@
+package practise;
+
+import org.testng.annotations.Test;
+
+public class TC_001 {
+	@Test
+	public void sample() {
+		System.out.println("Sample for pushing to local branch");
+	}
+}
